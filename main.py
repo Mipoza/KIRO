bf86ac570fa2b589c1b52e6ba4c319eee746b8f1
sinodine <sixtine.nodet@gmail.com>
2023-11-09 17:57:48 +0100

    calculer la solution naive

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from parser import parse_instance
 from map import draw_data
+from cost import const_cost
+from solution import solution_naive
 
 
 def main():
-    file = "instances/small.json"
+    file = "tiny.json"
     instance = parse_instance(file)
 
-    draw_data(instance)
+    # draw_data(instance)
+
+    x, y, z, I = solution_naive(instance)
+
+    print(const_cost(x, y, z, I))
 
 
 main()
--- solution.py
+++ solution.py
@@ -1,4 +1,25 @@
-def solution_naive():
+from collections import defaultdict
+
+
+def solution_naive(I):
     """
     La solution naive c'est toutes les turbines sur une sous station
     """
+
+    V_s = I[3]
+    V_t = I[2]
+
+    # construire la sous station 1 avec le type 1
+    x = defaultdict(int)
+    x[(1, 1)] = 1
+
+    # relier toutes les turbine à la sous station 1
+    z = defaultdict(int)
+    for t in V_t:
+        z[(1, t)] = 1
+
+    # relier la sous station 1 à la station de terre avec un cable de type 1
+    y = [[], []]
+    y[0] = [(1, 1)]
+
+    return (x, y, z, I)
