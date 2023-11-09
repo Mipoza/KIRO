e371a2cc97ca519f7497a6bb2af5c34c5e1d2328
sinodine <sixtine.nodet@gmail.com>
2023-11-09 16:55:23 +0100

    organize files

---
+++ map.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler  # To normalize features
+
+from sklearn.cluster import AgglomerativeClustering  # For HAC
+from sklearn.cluster import KMeans  # For K-means
+
+import geopandas as gpd  # For maps
