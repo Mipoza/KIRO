2b59166fb63d82c7c9480032a3a5dce1085dbdee
sinodine <sixtine.nodet@gmail.com>
2023-11-10 00:12:14 +0100

    fix parser

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import json
 from collections import defaultdict
 from math import sqrt
 
+
 def parse_instance(file):
     """
     Parse the input file and return a list of parameters and data structures needed for the optimization problem.
@@ -17,7 +18,7 @@ def parse_instance(file):
         - A dictionary of cable types for substation-substation connections, where each key is a cable type ID and each value is a list containing the fixed cost, rating, and variable cost of the cable.
     :rtype: list
     """
-    f = open('instances/'+file)
+    f = open("instances/" + file)
     data = json.load(f)
 
     gen_p = data["general_parameters"]
@@ -59,15 +60,18 @@ def parse_instance(file):
         for cbts in cabletype_subsub
     }
 
-    cabletype_subsub = data['substation_substation_cable_types'] #never fail
-    Q_s = {cbts['id']: [cbts['fixed_cost'], cbts['rating'], cbts['variable_cost']] for cbts in cabletype_subsub}
-    
-    wind_sen = data['wind_scenarios']
-    omega = {s['id']: [s['probability'], s['power_generation']] for s in wind_sen}
+    cabletype_subsub = data["substation_substation_cable_types"]  # never fail
+    Q_s = {
+        cbts["id"]: [cbts["fixed_cost"], cbts["rating"], cbts["variable_cost"]]
+        for cbts in cabletype_subsub
+    }
+
+    wind_sen = data["wind_scenarios"]
+    omega = {s["id"]: [s["probability"], s["power_generation"]] for s in wind_sen}
 
     f.close()
 
-    return [[c_0,c_p,c_ft,v_0,c_max,p_max,c_lt],S,V_t,V_s,Q_0,Q_s,omega]
+    return [[c_0, c_p, c_ft, v_0, c_max, p_max, c_lt], S, V_t, V_s, Q_0, Q_s, omega]
 
 
 def find_type(liste_de_couples, i_cherche):
@@ -76,7 +80,8 @@ def find_type(liste_de_couples, i_cherche):
             return k
     return None
 
-def save_sol(x,y,z,I,i):
+
+def save_sol(x, y, z, I, name):
     V_s = I[3]
     V_t = I[2]
     S = I[1]
@@ -84,40 +89,39 @@ def save_sol(x,y,z,I,i):
     Q_s = I[5]
     omega = I[6]
 
-    file_path = "solutions/sol"+str(i) +".json"
+    file_path = "solutions/" + name + ".json"
 
     data = {}
 
-    data['substations'] = []
+    data["substations"] = []
 
     for key, value in x.items():
-        d_s = {}
-        d_s['id'] = key[0]
-        t = find_type(y[0],key[0])
-        if None != t:
-            d_s['land_cable_type'] = find_type(y[0],key[0])
-            d_s['substation_type'] = key[1]
-            data['substations'].append(d_s)
+        if value == 1:
+            d_s = {}
+            d_s["id"] = key[0]
+            d_s["land_cable_type"] = find_type(y[0], key[0])
+            d_s["substation_type"] = key[1]
+            data["substations"].append(d_s)
 
-    data['turbines'] = []
-
-    for key, value in z.items():
-        d_t = {}
-        d_t['id'] = key[0]
-        d_t['substation_id'] = key[1]
-        data['turbines'].append(d_t)
-    
-    data['substation_substation_cables'] = []
+    data["substation_substation_cables"] = []
 
     for c in y[1]:
         d_s = {}
-        d_s['substation_id'] = c[0]
-        d_s['other_substation_id'] = c[1]
-        d_s['cable_type'] = c[3]
-        data['substation_substation_cables'].append(d_s)
+        d_s["substation_id"] = c[0]
+        d_s["other_substation_id"] = c[1]
+        d_s["cable_type"] = c[3]
+        data["substation_substation_cables"].append(d_s)
 
+    data["turbines"] = []
 
-    with open(file_path, 'w') as json_file:
+    for key, value in z.items():
+        d_t = {}
+        d_t["id"] = key[1]
+        d_t["substation_id"] = key[0]
+        data["turbines"].append(d_t)
+
+    with open(file_path, "w") as json_file:
         json.dump(data, json_file)
 
-print(parse_instance('tiny.json'))
+
+# print(parse_instance("tiny.json"))
