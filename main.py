58a36027bf05e96ad792ff4c9eede992354851db
sinodine <sixtine.nodet@gmail.com>
2023-11-26 20:32:45 +0100

    ok

--- main.py
+++ main.py
@@ -30,32 +30,32 @@ def main():
     scores = [10129655, 1340062, 1334244, 1321105, 1295076 ]
 
     """
-    file = "huge.json"
+    file = "small.json"
     I = parse_instance(file)
     I2 = parse_instance2(file)
 
     # display_data(I)
-    display_type_ss(I)
-    display_type_cable_land_ss(I)
+    # display_type_ss(I)
+    # display_type_cable_land_ss(I)
 
     name = "KIRO-" + file[:-5]
 
-    # tic = time.time()
-    # x, y, z, I = solution_naive(I)
-    # tac = time.time()
-    # print("tic-tac= ", tac - tic, " s")
-    # print("cout tot pour solution_naive= ", total_cost(x, y, z, I))
-    # display_solution(x, y, z, I)
-    # save_sol(x, y, z, name + "-naive")
-
     tic = time.time()
-    x, y, z, I2 = solution_naive2(I2)
+    x, y, z, I = solution_naive(I)
     tac = time.time()
     print("tic-tac= ", tac - tic, " s")
-    cout = total_cost(x, y, z, I)
-    print("cout tot pour solution_naive2= ", cout)
-    # # display_solution(x, y, z, I)
-    save_sol(x, y, z, name + "-naive2-2")
+    print("cout tot pour solution_naive= ", total_cost(x, y, z, I))
+    display_solution(x, y, z, I)
+    save_sol(x, y, z, name + "-naive")
+
+    # tic = time.time()
+    # x, y, z, I2 = solution_naive2(I2)
+    # tac = time.time()
+    # print("tic-tac= ", tac - tic, " s")
+    # cout = total_cost(x, y, z, I)
+    # print("cout tot pour solution_naive2= ", cout)
+    # display_solution(x, y, z, I)
+    # save_sol(x, y, z, name + "-naive2-2")
 
 
 main()
