8e0573ea0c8611939b17405deed016cfaefa2e1c
sinodine <sixtine.nodet@gmail.com>
2024-01-13 16:46:16 +0100

    terminado de traduire mon algo en julia

--- main.jl
+++ main.jl
@@ -5,6 +5,12 @@ include("instance.jl")
 include("solution.jl")
 include("parsing.jl")
 include("eval.jl")
+include("sixtine_sol.jl")
 
-I = read_instance("instances/KIRO-small.json")
-print(I.fixed_cost_cable)
+
+I = read_instance("instances/KIRO-huge.json")
+
+sol::Solution = resolution_sixtine(I)
+write_solution(sol, "solutions/solution.json")
+print(is_feasible(sol, I))
+print(cost(sol, I))
--- sixtine_sol.jl
+++ sixtine_sol.jl
@@ -7,6 +7,26 @@ include("solution.jl")
 include("parsing.jl")
 include("eval.jl")
 
+function lines(wind_turbines::Vector{Location})
+    """
+    Cette fonction renvoie les lignes de turbines i.e y = constante
+    """
+    turbines = sort(wind_turbines, by=x -> x.y)
+    lines = []
+    line = []
+    y = turbines[1].y
+    for turbine in turbines
+        if turbine.y == y
+            push!(line, turbine)
+        else
+            push!(lines, line)
+            line = [turbine]
+            y = turbine.y
+        end
+    end
+    push!(lines, line)
+    return lines
+end
 
 function choix_ss(turbine::Location, substation_locations::Vector{Location})
     """
@@ -15,9 +35,9 @@ function choix_ss(turbine::Location, substation_locations::Vector{Location})
     """
 
     mini = 100000
-    id_substation = substation_locations[-1].id
+    id_substation = substation_locations[end].id
     for substation in substation_locations
-        d = dist(turbine, substation)
+        d = distance(turbine, substation)
         if d < mini
             mini = d
             id_substation = substation.id
@@ -33,11 +53,11 @@ function choix_type_ss(substation_types::Vector{SubStationType})
     C'est actuellement indépendant de la ss ou du nombre de lignes de turbines reliées à la ss.
     """
 
-    id_type_ss = substation_types[-1].id
-    mini = substation_types[-1].probability_of_failure
+    id_type_ss = substation_types[end].id
+    mini = substation_types[end].probability_of_failure
 
-    while id_type_ss > 1 && substation_types[id_type_ss - 1].probability_of_failure <= mini
-        mini = substation_types[id_type_ss - 1].probability_of_failure
+    while id_type_ss > 1 && substation_types[id_type_ss-1].probability_of_failure <= mini
+        mini = substation_types[id_type_ss-1].probability_of_failure
         id_type_ss -= 1
     end
     return id_type_ss
@@ -48,16 +68,16 @@ function choix_type_cable_land_ss(land_substation_cable_types::Vector{CableType}
     On choisit min failure et min cout total
     """
 
-    id_type_cable = land_substation_cable_types[-1].id
-    mini = land_substation_cable_types[-1].probability_of_failure + land_substation_cable_types[-1].variable_cost
-    while id_type_cable > 1 && land_substation_cable_types[id_type_cable - 1].probability_of_failure + land_substation_cable_types[id_type_cable - 1].variable_cost <= mini
-        mini = land_substation_cable_types[id_type_cable - 1].probability_of_failure + land_substation_cable_types[id_type_cable - 1].variable_cost
+    id_type_cable = land_substation_cable_types[end].id
+    mini = land_substation_cable_types[end].probability_of_failure + land_substation_cable_types[end].variable_cost
+    while id_type_cable > 1 && land_substation_cable_types[id_type_cable-1].probability_of_failure + land_substation_cable_types[id_type_cable-1].variable_cost <= mini
+        mini = land_substation_cable_types[id_type_cable-1].probability_of_failure + land_substation_cable_types[id_type_cable-1].variable_cost
         id_type_cable -= 1
-    end    
-    return type_cable
+    end
+    return id_type_cable
 end
 
-function solution(instance::Instance)
+function resolution_sixtine(instance::Instance)
     """
     Cette solution consiste à mettre toutes les turbines d'une même ligne sur une même sous station la plus proche.
 
@@ -69,38 +89,37 @@ function solution(instance::Instance)
     - relier les sous stations entre elles
     - optimiser sur le type de cable choisi pour relier les sous stations entre elles
     """
-    V_s = I.substation_locations
-    V_t = I.wind_turbines
-    S = I.substation_types
-    Q_0 = I.land_substation_cable_types
 
-    ss_visitees = []
 
-    z = Dict()
+    substations = []
+    inter_station_cables = zeros(Int, nb_station_locations(instance), nb_station_locations(instance))
+    turbine_links = zeros(nb_turbines(instance))
 
-    for line in V_t
-        id_ss = choix_ss(first(values(first(line))), V_s)
+    ss_visited = []
+    lines_turbines = lines(instance.wind_turbines)
 
-        ss_visitees = [ss_visitees..., id_ss]
+    # On choisit un type de ss ( substation_types)
+    # On choisit un type de cable pour relier une station à la terre (land_substation_cable_types)
+    # pour chaque ligne de turbines
+    # --- on choisit une ss (la plus proche de la première turbine de la ligne)
+    # --- on ajoute la ss à la liste des ss visitées
+    # --- pour chaque turbine de la ligne de turbines
+    # --- --- On relie la turbine à la ss : turbine_links[turbine.id] = ss.id
+    # Pour chaque ss visitée
+    # --- On construit la ss : substations.append(Substation(id, substation_type, land_cable_type))
+    # Renvoyer la solution
 
-        for t in line
-            z[(id_ss, first(keys(t)))] = 1
+    type_ss = choix_type_ss(instance.substation_types)
+    type_cable = choix_type_cable_land_ss(instance.land_substation_cable_types)
+    for line_turbines in lines_turbines
+        id_substation = choix_ss(first(line_turbines), instance.substation_locations)
+        ss_visited = [ss_visited..., id_substation]
+        for turbine in line_turbines
+            turbine_links[turbine.id] = id_substation
         end
     end
-
-    x = Dict()
-    y = [[], []]
-
-    type_ss = choix_type_ss(S)
-    type_cable = choix_type_cable_land_ss(Q_0)
-
-    println("type_ss= ", type_ss)
-    println("type_cable= ", type_cable)
-
-    for s in ss_visitees
-        x[(s, type_ss)] = 1
-        push!(y[1], (s, type_cable))
+    for substation in ss_visited
+        push!(substations, SubStation(substation, type_ss, type_cable))
     end
-
-    return (x, y, z, I)
+    return Solution(; turbine_links, inter_station_cables, substations)
 end
