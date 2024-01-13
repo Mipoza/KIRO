bd263fd2fd679ad3b0a0d6f4e3fe3add708fe1a6
sinodine <sixtine.nodet@gmail.com>
2024-01-13 15:50:41 +0100

    modif sixtine_sol pour tout sauf la fonction main

--- sixtine_sol.jl
+++ sixtine_sol.jl
@@ -7,99 +7,61 @@ include("solution.jl")
 include("parsing.jl")
 include("eval.jl")
 
-function dist(s, t)
-    return sqrt((s[1] - t[1])^2 + (s[2] - t[2])^2)
-end
 
-function choix_ss_in_line(t_pos, line_ss)
-    """
-    Cette fonction choisit la sous station à laquelle on va relier la ligne de turbine dans la ligne de ss donnée.
-    L'heuristique actuelle est de prendre la dernière ss de la ligne.
-    """
-    x = 0
-    ss0 = line_ss[1]
-    for ss in line_ss
-        pos_ss = first(values(ss))
-        if pos_ss[1] > x
-            x = pos_ss[1]
-            ss0 = ss
-        end
-    end
-    return first(keys(ss0))
-end
-
-function choix_ss_in_line2(t_pos, line_ss)
-    """
-    Cette fonction choisit la sous station à laquelle on va relier la ligne de turbine dans la ligne de ss donnée.
-    L'heuristique actuelle est de prendre la première ss de la ligne.
-    """
-    x = 1000
-    ss0 = line_ss[1]
-    for ss in line_ss
-        pos_ss = first(values(ss))
-        if pos_ss[1] < x
-            x = pos_ss[1]
-            ss0 = ss
-        end
-    end
-    return first(keys(ss0))
-end
-
-function choix_ss(t_pos, V_s)
+function choix_ss(turbine::Location, substation_locations::Vector{Location})
     """
     Cette fonction choisit la sous station à laquelle on va relier la ligne de turbine.
-    L'heuristique actuelle est de prendre la dernière ss de la ligne la plus proche de la turbine.
-
-    t_pos est la position de la turbine de la ligne
+    L'heuristique actuelle est de prendre la ss la plus proche de la turbine.
     """
+
     mini = 100000
-    s2 = first(keys(V_s[1][end]))
-    for line_ss in V_s
-        s3 = choix_ss_in_line2(t_pos, line_ss)
-        d = dist(t_pos, first(values(s3)))
+    id_substation = substation_locations[-1].id
+    for substation in substation_locations
+        d = dist(turbine, substation)
         if d < mini
             mini = d
-            s2 = first(keys(s3))
+            id_substation = substation.id
         end
     end
-    return s2
+    return id_substation
 end
 
-function choix_type_ss(S)
+function choix_type_ss(substation_types::Vector{SubStationType})
     """
     Cette fonction choisit le type de sous station à laquelle on va relier la ligne de turbine.
     L'heuristique actuelle est de prendre le risque de failure le plus grand et de cout minimal.
     C'est actuellement indépendant de la ss ou du nombre de lignes de turbines reliées à la ss.
     """
-    type_ss = keys(S[end])[1]
-    mini = S[end][1]
 
-    while type_ss > 1 && S[type_ss - 1][1] <= mini
-        mini = S[type_ss - 1][1]
-        type_ss -= 1
+    id_type_ss = substation_types[-1].id
+    mini = substation_types[-1].probability_of_failure
+
+    while id_type_ss > 1 && substation_types[id_type_ss - 1].probability_of_failure <= mini
+        mini = substation_types[id_type_ss - 1].probability_of_failure
+        id_type_ss -= 1
     end
-    return type_ss
+    return id_type_ss
 end
 
-function choix_type_cable_land_ss(Q_0)
+function choix_type_cable_land_ss(land_substation_cable_types::Vector{CableType})
     """
     On choisit min failure et min cout total
     """
-    type_cable = keys(Q_0[end])[1]
-    mini = Q_0[end][1] + Q_0[end][3]
-    while type_cable > 1 && Q_0[type_cable - 1][1] + Q_0[type_cable - 1][3] <= mini
-        mini = Q_0[type_cable - 1][1] + Q_0[type_cable - 1][3]
-        type_cable -= 1
-    end
+
+    id_type_cable = land_substation_cable_types[-1].id
+    mini = land_substation_cable_types[-1].probability_of_failure + land_substation_cable_types[-1].variable_cost
+    while id_type_cable > 1 && land_substation_cable_types[id_type_cable - 1].probability_of_failure + land_substation_cable_types[id_type_cable - 1].variable_cost <= mini
+        mini = land_substation_cable_types[id_type_cable - 1].probability_of_failure + land_substation_cable_types[id_type_cable - 1].variable_cost
+        id_type_cable -= 1
+    end    
     return type_cable
 end
 
-function solution_naive2(I)
+function solution(instance::Instance)
     """
     Cette solution consiste à mettre toutes les turbines d'une même ligne sur une même sous station la plus proche.
 
-    I2 est l'instance obtenue avec la fonction parse_instance2 du fichier parser.py
-
+    parser selon lignes
     Piste d'amélioration:
     - optimiser sur le type de station choisi par défault
     - optimiser sur le type de cable choisi par défault
